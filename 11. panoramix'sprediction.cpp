c868549451a1c591ede5c7a918b855fe0bb6f5f7
Yashtika Kakkar <43854410+yashtikakakkar@users.noreply.github.com>
2020-10-17 19:01:49 +0530

    Update 11. panoramix'sprediction.cpp

--- 11. panoramix'sprediction.cpp
+++ 11. panoramix'sprediction.cpp
@@ -1,10 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int main()
 {
     int i, n, m, ans=0;
-    cin>>n>>m;
-    for(i=n+1 ; i<=m; i++)
+    cin >> n >> m;
+    for(i=n+1; i<=m; i++)
     {
         int flag=0;
         for(int k=2; k<i; k++)
@@ -14,22 +15,24 @@ int main()
                 flag=1;
                 break;
             }
-            else continue;
+            else 
+                continue;
         }
-        if(flag == 0 && i==m){
-            cout<<"YES";
+        if (flag == 0 && i==m){
+            cout << "YES";
             ans=1;
             break;
         }
         else if(flag == 0 && i!=m)
         {
-            cout<<"NO";
+            cout << "NO";
             ans=1;
             break;
         }
-        else continue;
+        else 
+            continue;
     }
-    if(ans==0)
+    if (ans==0)
         cout<<"NO";
     return 0;
 }
