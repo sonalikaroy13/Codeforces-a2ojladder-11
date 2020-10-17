efa8317ca39d415a18c1ee65172e13539f48b717
Yashtika Kakkar <43854410+yashtikakakkar@users.noreply.github.com>
2020-10-17 18:59:47 +0530

    Update 10. stonesonthetable.cpp

--- 10. stonesonthetable.cpp
+++ 10. stonesonthetable.cpp
@@ -1,21 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int main()
 {
     int i, n, count=0;
     string s;
-    cin>>n>>s;
-    for(i=1 ; i<n; i++)
+    cin >> n >> s;
+    for (i=1; i<n; i++)
     {
-        if(s[i]==s[i-1])
-        {
+        if (s[i]==s[i-1])
             count++;
-        }
         else
-        {
             continue;
-        }
     }
-    cout<<count;
+    cout << count;
     return 0;
 }
